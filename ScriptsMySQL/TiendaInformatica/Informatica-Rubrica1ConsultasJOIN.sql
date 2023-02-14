0c5ffe6b40374692b4783eb2bcc41d8a091102c3
Alberto Saz <albertosaz@gmail.com>
2023-02-14 16:23:22 +0100

    Mudanza a GitHub con nuevos Script SQL avanzados

---
+++ ScriptsMySQL/TiendaInformatica/Informatica-Rubrica1ConsultasJOIN.sql
@@ -0,0 +1,39 @@
+# 1.1.4 Consultas multitabla (Composición interna)
+#
+# Resuelva todas las consultas utilizando la sintaxis de SQL1 y SQL2.
+#
+# Devuelve una lista con el nombre del producto, precio y nombre de fabricante de todos los productos de la base de datos.
+#
+# Devuelve una lista con el nombre del producto, precio y nombre de fabricante de todos los productos de la base de datos. Ordene el resultado por el nombre del fabricante, por orden alfabético.
+#
+# Devuelve una lista con el identificador del producto, nombre del producto, identificador del fabricante y nombre del fabricante, de todos los productos de la base de datos.
+#
+# Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más barato.
+#
+# Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más caro.
+#
+# Devuelve una lista de todos los productos del fabricante Lenovo.
+#
+# Devuelve una lista de todos los productos del fabricante Crucial que tengan un precio mayor que 200€.
+#
+# Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packardy Seagate. Sin utilizar el operador IN.
+#
+# Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packardy Seagate. Utilizando el operador IN.
+#
+# Devuelve un listado con el nombre y el precio de todos los productos de los fabricantes cuyo nombre termine por la vocal e.
+#
+# Devuelve un listado con el nombre y el precio de todos los productos cuyo nombre de fabricante contenga el carácter w en su nombre.
+#
+# Devuelve un listado con el nombre de producto, precio y nombre de fabricante, de todos los productos que tengan un precio mayor o igual a 180€. Ordene el resultado en primer lugar por el precio (en orden descendente) y en segundo lugar por el nombre (en orden ascendente)
+#
+# Devuelve un listado con el identificador y el nombre de fabricante, solamente de aquellos fabricantes que tienen productos asociados en la base de datos.
+#
+# 1.1.5 Consultas multitabla (Composición externa)
+#
+# Resuelva todas las consultas utilizando las cláusulas LEFT JOIN y RIGHT JOIN.
+#
+# Devuelve un listado de todos los fabricantes que existen en la base de datos, junto con los productos que tiene cada uno de ellos. El listado deberá mostrar también aquellos fabricantes que no tienen productos asociados.
+#
+# Devuelve un listado donde sólo aparezcan aquellos fabricantes que no tienen ningún producto asociado.
+#
+# ¿Pueden existir productos que no estén relacionados con un fabricante? Justifique su respuesta.
