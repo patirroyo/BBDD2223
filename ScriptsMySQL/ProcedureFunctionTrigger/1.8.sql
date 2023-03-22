fc4a37fc13d8e5e02beb2d73f00f5b282e61da9f
Alberto Saz <albertosaz@gmail.com>
2023-03-22 09:45:52 +0100

    Un ejercicio y medio

--- ScriptsMySQL/ProcedureFunctionTrigger/1.8.sql
+++ ScriptsMySQL/ProcedureFunctionTrigger/1.8.sql
@@ -1,30 +1,30 @@
 # 1.8.1 Procedimientos sin sentencias SQL
--- Lo primero que hay que hacer es cambiar el delimitador:
-DELIMITER $$
--- Luego CREATE PROCEDURE, BEGIN y termino con END;
-CREATE PROCEDURE listar_productos(IN gama VARCHAR(50))
-    -- IN paso por valor, metes el valor en tu código y haces lo que quieres con él pero sin modificar el dato original, es decir se pasa una copia.
-    -- INOUT paso por referencia: si se modifica el dato, se modifica el valor original, se pasará el valor original, no una copia.
-    -- OUT son variables de salida, variables que quiero que contengan información. Como devolver un valor, simplemente las declaro (Declare) y ya las puedo usar.
-        -- DECLARE gama;
-        -- CALL listar_productos(gama);
-
-BEGIN
-    SELECT *
-    FROM producto
-    WHERE producto.gama = gama;
-END $$
--- Cuando acabamos lo volvemos a cambiar:
-DELIMITER ;
--- llamamos al procedimiento
-CALL listar_productos('Frutales');
-
 
 # Escribe un procedimiento que no tenga ningún parámetro de entrada ni de salida y que muestre el texto ¡Hola mundo!.
 
+DELIMITER $$
+DROP PROCEDURE IF EXISTS hello_world $$
 
+CREATE PROCEDURE hello_world()
+BEGIN
+    SELECT '¡Hola mundo!';
+END $$
+
+DELIMITER ;
+CALL hello_world();
 
 # Escribe un procedimiento que reciba un número real de entrada y muestre un mensaje indicando si el número es positivo, negativo o cero.
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS signo_numero $$
+
+CREATE PROCEDURE signo_numero(IN numero INT)
+
+    CASE
+        WHEN numero > 0 THEN SELECT 'El número es positivo';
+        WHEN numero < 0 THEN SELECT 'El número es negativo';
+        ELSE SELECT 'El número es cero'
+    END CASE $$
 
 # Modifique el procedimiento diseñado en el ejercicio anterior para que tenga un parámetro de entrada, con el valor un número real, y un parámetro de salida, con una cadena de caracteres indicando si el número es positivo, negativo o cero.
 
@@ -35,52 +35,127 @@ CALL listar_productos('Frutales');
 # [7, 9) = Notable
 # [9, 10] = Sobresaliente
 # En cualquier otro caso la nota no será válida.
+
+
 # Modifique el procedimiento diseñado en el ejercicio anterior para que tenga un parámetro de entrada, con el valor de la nota en formato numérico y un parámetro de salida, con una cadena de texto indicando la nota correspondiente.
+
+
+
 # Resuelva el procedimiento diseñado en el ejercicio anterior haciendo uso de la estructura de control CASE.
+
+
+
 # Escribe un procedimiento que reciba como parámetro de entrada un valor numérico que represente un día de la semana y que devuelva una cadena de caracteres con el nombre del día de la semana correspondiente. Por ejemplo, para el valor de entrada 1 debería devolver la cadena lunes.
+
+
 # 1.8.2 Procedimientos con sentencias SQL
-#
+
 # Escribe un procedimiento que reciba el nombre de un país como parámetro de entrada y realice una consulta sobre la tabla cliente para obtener todos los clientes que existen en la tabla de ese país.
+
+
+
 # Escribe un procedimiento que reciba como parámetro de entrada una forma de pago, que será una cadena de caracteres (Ejemplo: PayPal, Transferencia, etc). Y devuelva como salida el pago de máximo valor realizado para esa forma de pago. Deberá hacer uso de la tabla pago de la base de datos jardineria.
+
+
+
 # Escribe un procedimiento que reciba como parámetro de entrada una forma de pago, que será una cadena de caracteres (Ejemplo: PayPal, Transferencia, etc). Y devuelva como salida los siguientes valores teniendo en cuenta la forma de pago seleccionada como parámetro de entrada:
 # el pago de máximo valor,
+
+
 # el pago de mínimo valor,
+
+
 # el valor medio de los pagos realizados,
+
+
 # la suma de todos los pagos,
+
+
 # el número de pagos realizados para esa forma de pago.
+
+
 # Deberá hacer uso de la tabla pago de la base de datos jardineria.
-#
+
+
+
+
 # Crea una base de datos llamada procedimientos que contenga una tabla llamada cuadrados. La tabla cuadrados debe tener dos columnas de tipo INT UNSIGNED, una columna llamada número y otra columna llamada cuadrado.
+
+
+
 # Una vez creada la base de datos y la tabla deberá crear un procedimiento llamado calcular_cuadrados con las siguientes características. El procedimiento recibe un parámetro de entrada llamado tope de tipo INT UNSIGNED y calculará el valor de los cuadrados de los primeros números naturales hasta el valor introducido como parámetro. El valor del números y de sus cuadrados deberán ser almacenados en la tabla cuadrados que hemos creado previamente.
 #
 # Tenga en cuenta que el procedimiento deberá eliminar el contenido actual de la tabla antes de insertar los nuevos valores de los cuadrados que va a calcular.
 #
+
+
 # Utilice un bucle WHILE para resolver el procedimiento.
 #
+
+
 # Utilice un bucle REPEAT para resolver el procedimiento del ejercicio anterior.
+
+
 # Utilice un bucle LOOP para resolver el procedimiento del ejercicio anterior.
+
+
+
+
 # Crea una base de datos llamada procedimientos que contenga una tabla llamada ejercicio. La tabla debe tener una única columna llamada número y el tipo de dato de esta columna debe ser INT UNSIGNED.
+
+
+
 # Una vez creada la base de datos y la tabla deberá crear un procedimiento llamado calcular_números con las siguientes características. El procedimiento recibe un parámetro de entrada llamado valor_inicial de tipo INT UNSIGNED y deberá almacenar en la tabla ejercicio toda la secuencia de números desde el valor inicial pasado como entrada hasta el 1.
-#
+
 # Tenga en cuenta que el procedimiento deberá eliminar el contenido actual de las tablas antes de insertar los nuevos valores.
-#
+
+
 # Utilice un bucle WHILE para resolver el procedimiento.
-#
+
+
+
 # Utilice un bucle REPEAT para resolver el procedimiento del ejercicio anterior.
+
+
+
 # Utilice un bucle LOOP para resolver el procedimiento del ejercicio anterior.
+
+
+
 # Crea una base de datos llamada procedimientos que contenga una tabla llamada pares y otra tabla llamada impares. Las dos tablas deben tener única columna llamada número y el tipo de dato de esta columna debe ser INT UNSIGNED.
+
+
+
+
 # Una vez creada la base de datos y las tablas deberá crear un procedimiento llamado calcular_pares_impares con las siguientes características. El procedimiento recibe un parámetro de entrada llamado tope de tipo INT UNSIGNED y deberá almacenar en la tabla pares aquellos números pares que existan entre el número 1 el valor introducido como parámetro. Habrá que realizar la misma operación para almacenar los números impares en la tabla impares.
-#
+
+
+
 # Tenga en cuenta que el procedimiento deberá eliminar el contenido actual de las tablas antes de insertar los nuevos valores.
-#
+
+
+
+
 # Utilice un bucle WHILE para resolver el procedimiento.
-#
+
+
 # Utilice un bucle REPEAT para resolver el procedimiento del ejercicio anterior.
+
+
 # Utilice un bucle LOOP para resolver el procedimiento del ejercicio anterior.
+
+
+
 # 1.8.3 Funciones sin sentencias SQL
-#
+
 # Escribe una función que reciba un número entero de entrada y devuelva TRUE si el número es par o FALSE en caso contrario.
+
+
+
 # Escribe una función que devuelva el valor de la hipotenusa de un triángulo a partir de los valores de sus lados.
+
+
+
 # Escribe una función que reciba como parámetro de entrada un valor numérico que represente un día de la semana y que devuelva una cadena de caracteres con el nombre del día de la semana correspondiente. Por ejemplo, para el valor de entrada 1 debería devolver la cadena lunes.
 # Escribe una función que reciba tres números reales como parámetros de entrada y devuelva el mayor de los tres.
 # Escribe una función que devuelva el valor del área de un círculo a partir del valor del radio que se recibirá como parámetro de entrada.
@@ -96,6 +171,9 @@ CALL listar_productos('Frutales');
 # Escribe una función para la base de datos tienda que devuelva el valor medio del precio de los productos de un determinado fabricante que se recibirá como parámetro de entrada. El parámetro de entrada será el nombre del fabricante.
 # Escribe una función para la base de datos tienda que devuelva el valor máximo del precio de los productos de un determinado fabricante que se recibirá como parámetro de entrada. El parámetro de entrada será el nombre del fabricante.
 # Escribe una función para la base de datos tienda que devuelva el valor mínimo del precio de los productos de un determinado fabricante que se recibirá como parámetro de entrada. El parámetro de entrada será el nombre del fabricante.
+
+
+
 # 1.8.5 Manejo de errores en MySQL
 #
 # Crea una base de datos llamada test que contenga una tabla llamada alumno. La tabla debe tener cuatro columnas:
