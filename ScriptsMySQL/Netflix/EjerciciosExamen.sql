2c48e73fcb880f5e3aeffcd21d4e16bcd0c817b4
Alberto Saz <albertosaz@gmail.com>
2023-05-04 23:55:02 +0200

    Ejercicios Examen y archivo importacion Netflix (shakila castellano)

---
+++ ScriptsMySQL/Netflix/EjerciciosExamen.sql
@@ -0,0 +1,70 @@
+# 1)Se desea tener un procedimiento almacenado tal que dados un nombre y apellidos del empleado, compruebe si este empleado es jefe de un almacén.
+
+DELIMITER $$
+DROP FUNCTION IF EXISTS es_jefe $$
+CREATE FUNCTION es_jefe(nombre VARCHAR(45), apellidos VARCHAR(45))
+    RETURNS BOOLEAN DETERMINISTIC
+    BEGIN
+        DECLARE id TINYINT UNSIGNED;
+        SET id = (SELECT e.id_empleado
+                FROM empleado e
+                WHERE e.nombre = nombre
+                    AND e. apellidos = apellidos);
+        IF id IN (SELECT a.id_empleado_jefe
+                    FROM almacen a) THEN
+            RETURN TRUE;
+        ELSE
+            RETURN FALSE;
+        END IF;
+    END $$
+DELIMITER ;
+SELECT es_jefe('Ringo', 'Rooksby');
+SELECT es_jefe('Ada', 'Byron');
+
+
+# 2)En caso afirmativo deberemos recuperar todos los empleados de ese almacén.
+
+
+
+# 3)De cada empleado de ese almacén calcularemos  el tiempo medio de devolución de los alquileres de ese empleado.
+# 4)Al final del procedimiento devolveremos la suma de todos los tiempos medios de devolución de alquileres(apartado 3)
+#
+DELIMITER $$
+DROP PROCEDURE IF EXISTS tiempo_medio_devolucion_por_jefe $$
+CREATE PROCEDURE tiempo_medio_devolucion_por_jefe(IN nombre VARCHAR(45), IN apellidos VARCHAR(45), OUT sum_avg_time INTEGER)
+    BEGIN
+        DECLARE done INT DEFAULT 0;
+        DECLARE id_jefe TINYINT UNSIGNED;
+        DECLARE id_almacen TINYINT UNSIGNED;
+        DECLARE empleados_almacen CURSOR FOR SELECT id_empleado
+                                    FROM empleado e
+                                    WHERE e.id_almacen = id_almacen;
+        DECLARE id_e TINYINT UNSIGNED;
+        DECLARE average_per_emp INTEGER;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+        SET id_jefe = (SELECT e.id_empleado
+                        FROM empleado e
+                        WHERE e.nombre = nombre
+                            AND e.apellidos = apellidos);
+        SET id_almacen = (SELECT a.id_almacen
+                            FROM almacen a
+                            WHERE a.id_empleado_jefe = id_jefe
+                            LIMIT 1);
+        OPEN empleados_almacen;
+            bucle: LOOP
+                FETCH empleados_almacen
+                INTO id_e;
+                SET average_per_emp = 0;
+                IF done = 1 THEN
+                    LEAVE bucle;
+                END IF;
+                SELECT AVG(DATEDIFF(a.fecha_devolucion - a.fecha_alquiler))
+                    INTO average_per_emp
+                    FROM  alquiler a
+                    WHERE a.id_empleado = id_e
+                        AND a.fecha_devolucion > a.fecha_alquiler;
+                SET sum_avg_time = sum_avg_time + average_per_emp;
+            END LOOP bucle;
+    END $$
+DELIMITER ;
+CALL tiempo_medio_devolucion_por_jefe('Ringo', 'Rooksby');
