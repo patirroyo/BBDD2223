8b1db1d26c16f74a83bbeda045b9452f47dc816e
Alberto Saz <albertosaz@gmail.com>
2023-05-09 12:20:55 +0200

    Ejercicios Examen por fin!!!!

--- ScriptsMySQL/Netflix/EjerciciosExamen.sql
+++ ScriptsMySQL/Netflix/EjerciciosExamen.sql
@@ -36,12 +36,15 @@ CREATE PROCEDURE tiempo_medio_devolucion_por_jefe(IN nombre VARCHAR(45), IN apel
         DECLARE done INT DEFAULT 0;
         DECLARE id_jefe TINYINT UNSIGNED;
         DECLARE id_almacen TINYINT UNSIGNED;
+        DECLARE id_e TINYINT UNSIGNED;
+        DECLARE average_per_emp INTEGER;
+        DECLARE num_empleados INTEGER;
         DECLARE empleados_almacen CURSOR FOR SELECT id_empleado
                                     FROM empleado e
                                     WHERE e.id_almacen = id_almacen;
-        DECLARE id_e TINYINT UNSIGNED;
-        DECLARE average_per_emp INTEGER;
         DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+        SET sum_avg_time = 0;
         SET id_jefe = (SELECT e.id_empleado
                         FROM empleado e
                         WHERE e.nombre = nombre
@@ -50,6 +53,9 @@ CREATE PROCEDURE tiempo_medio_devolucion_por_jefe(IN nombre VARCHAR(45), IN apel
                             FROM almacen a
                             WHERE a.id_empleado_jefe = id_jefe
                             LIMIT 1);
+        SELECT COUNT(e.id_empleado) INTO num_empleados
+            FROM empleado e
+            WHERE e.id_almacen = id_almacen;
         OPEN empleados_almacen;
             bucle: LOOP
                 FETCH empleados_almacen
@@ -58,13 +64,23 @@ CREATE PROCEDURE tiempo_medio_devolucion_por_jefe(IN nombre VARCHAR(45), IN apel
                 IF done = 1 THEN
                     LEAVE bucle;
                 END IF;
-                SELECT AVG(DATEDIFF(a.fecha_devolucion - a.fecha_alquiler))
+                SELECT AVG(DATEDIFF(a.fecha_devolucion, a.fecha_alquiler))
                     INTO average_per_emp
                     FROM  alquiler a
                     WHERE a.id_empleado = id_e
                         AND a.fecha_devolucion > a.fecha_alquiler;
-                SET sum_avg_time = sum_avg_time + average_per_emp;
+                SELECT average_per_emp;
+                IF(average_per_emp is not null) THEN
+                    SET sum_avg_time = sum_avg_time + average_per_emp;
+                    SELECT sum_avg_time;
+                ELSE
+                    SET num_empleados = num_empleados - 1;
+                END IF;
+
             END LOOP bucle;
+        SET sum_avg_time = sum_avg_time / num_empleados;
+        CLOSE empleados_almacen;
     END $$
 DELIMITER ;
-CALL tiempo_medio_devolucion_por_jefe('Ringo', 'Rooksby');
+CALL tiempo_medio_devolucion_por_jefe('Jon', 'Stephens', @suma);
+SELECT @suma;
